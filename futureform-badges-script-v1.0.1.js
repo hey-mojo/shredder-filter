c8904adeaad2d9ea35b2e1d4ca5ea128c053b5c5
hey-mojo <sergei.heymojo@gmail.com>
2026-03-31 23:40:30 +0100

    Futureform

--- futureform-badges-script-v1.0.1.js
+++ futureform-badges-script-v1.0.1.js
@@ -1,13 +1,14 @@
 (() => {
-  const badgeImageUrl =
-    "https://eu.evocdn.io/dealer/1021/content/media/Fellowes Cashback Folder/";
+  const badgeImageUrl = "https://eu.evocdn.io/dealer/1021/content/media/Fellowes Cashback Folder/";
 
   const badgesInfo = [
+    { id: 6507, image_name: "Cashback-5-UK.png" },
     { id: 6067, image_name: "Cashback-8-UK.png" },
     { id: 6068, image_name: "Cashback-10-UK.png" },
     { id: 6069, image_name: "Cashback-15-UK.png" },
     { id: 6070, image_name: "Cashback-17-UK.png" },
     { id: 6015, image_name: "Cashback-20-UK.png" },
+    { id: 6400, image_name: "Cashback-25-UK.png" },
     { id: 6016, image_name: "Cashback-30-UK.png" },
     { id: 6017, image_name: "Cashback-35-UK.png" },
     { id: 6018, image_name: "Cashback-40-UK.png" },
@@ -16,6 +17,7 @@
     { id: 6021, image_name: "Cashback-55-UK.png" },
     { id: 6157, image_name: "Cashback-60-UK.png" },
     { id: 6022, image_name: "Cashback-80-UK.png" },
+    { id: 6401, image_name: "Cashback-90-UK.png" },
     { id: 6158, image_name: "Cashback-100-UK.png" },
     { id: 6023, image_name: "Cashback-105-UK.png" },
     { id: 6159, image_name: "Cashback-120-UK.png" },
